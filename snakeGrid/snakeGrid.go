522624dba244460d75ddf6aa06340be738f713ca
Collin Rettig <weatoria@gmail.com>
2017-10-16 23:41:18 -0400

    basic

---
+++ snakeGrid/snakeGrid.go
@@ -0,0 +1,60 @@
+components {
+  id: "grid"
+  component: "/graphics/grid.tilemap"
+  position {
+    x: 800.0
+    y: 200.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "snake"
+  component: "/main/snake.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "gameGui"
+  component: "/snakeGrid/gui/gameGui.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "camera"
+  component: "/camera/camera.camera"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
